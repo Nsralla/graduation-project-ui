ddfd97de85717331939e189223ec9a1874ab055a
Nsralla <nsrhassan22@gmail.com>
2024-10-04 19:58:50 +0300

    Refactor landing page component and styles

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    assetsInlineLimit: 16000, // 15 KB limit (increased from the default 4 KB)
+  },
+});
