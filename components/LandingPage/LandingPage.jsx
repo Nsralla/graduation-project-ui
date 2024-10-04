793a7c18fd068b537f724166e16c0e29f0889c37
Nsralla <nsrhassan22@gmail.com>
2024-10-04 20:10:26 +0300

    Refactor landing page component and styles

--- components/LandingPage/LandingPage.jsx
+++ components/LandingPage/LandingPage.jsx
@@ -2,23 +2,24 @@ import "./LandingPage.css";
 import hero_Video from "../../public/student_with_phone.mp4";
 const LandingPage = () => {
   return (
-     <div className="hero-container">
-        <video
-          className="hero-video"
-          src={hero_Video}
-          autoPlay
-          loop
-          muted
-          playsInline
-        />
-        <div className="hero-overlay">
-          <h1>Welcome to Our Landing Page</h1>
-          <p>Connecting Students with Knowledge</p>
-          <button>Get Started</button>
-        </div>
-       
+    <div className="hero-container">
+      <video
+        className="hero-video"
+        src={hero_Video}
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
+      <div className="hero-overlay">
+        <h1>Welcome to Our Landing Page</h1>
+        <p>Connecting Students with Knowledge</p>
+        <button>
+          Get Started <i className="fa-solid fa-arrow-right"></i>
+        </button>
       </div>
-  )
+    </div>
+  );
 }
 
 export default LandingPage
